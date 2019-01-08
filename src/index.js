4fa319d376dbdb70b19f1c2f8720046f6905d761
Cong Zhang <dancerphil1994@gmail.com>
2019-01-08 10:41:10 +0800

    0.6.1

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { getProvider, Region } from 'region-core';
 
 const Provider = getProvider();
-const region = new Region({ reducerPath: 'region' });
+const region = new Region('region');
 
 export const { setConfig, getLoading, getResults, getFetchTimes, mapResultToProps, set, load, connectWith, reducer } = region;
 
